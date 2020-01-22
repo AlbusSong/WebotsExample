d4ccfbf04a7937ed7d74f5b2db058316bcdf9e86
AlbusSong <strawberry1234561@gmail.com>
2020-01-22 21:21:54 +0200

    Initial commit

---
+++ WebotsExample/WebotsExample/Controller/e-puck_avoid_obstacles.h
@@ -0,0 +1,16 @@
+//
+//  e-puck_avoid_obstacles.h
+//  WebotsExample
+//
+//  Created by Albus on 1/22/20.
+//  Copyright © 2020 Albus. All rights reserved.
+//
+
+#ifndef e_puck_avoid_obstacles_h
+#define e_puck_avoid_obstacles_h
+
+#include <stdio.h>
+
+e_puck_avoid_obstacles_main(int argc, char **argv);
+
+#endif /* e_puck_avoid_obstacles_h */
---
+++ WebotsExample/WebotsExample/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  WebotsExample
+//
+//  Created by Albus on 1/22/20.
+//  Copyright © 2020 Albus. All rights reserved.
+//
+
+#include <stdio.h>
+#include "e-puck_avoid_obstacles.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return e_puck_avoid_obstacles_main(argv, argv);
+}
